5f5ad7ca08ed57bbf997b5f784446199d6729a9b
mestihudson <mestihudson@gmail.com>
2021-11-15 18:25:48 -0300

    feat: add errors and middlewares to common lib

---
+++ src/middlewares/current-user.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request, res: Response, next: NextFunction) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const currentUser = jwt
+      .verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
+    req.currentUser = currentUser;
+  } catch (err) {
+  }
+
+  next();
+};
