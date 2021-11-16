33ef48c8359dd0052d17b72bb9604bdbef99005c
mestihudson <mestihudson@gmail.com>
2021-11-16 20:04:33 -0300

    feat: improve error hendler logging

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -8,5 +8,6 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
   res.status(400).send({ errors: [ { message: 'Something went wrong' } ] });
 };
