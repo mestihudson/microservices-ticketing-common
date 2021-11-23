b4446b7e065ead49c72621f3ad18bafb7be45e3f
mestihudson <mestihudson@gmail.com>
2021-11-23 00:08:49 -0300

    fix: replace by correct filename

--- src/index.ts
+++ src/index.ts
@@ -17,4 +17,4 @@ export * from '@/events/subjects';
 export * from '@/events/ticket-created-event';
 export * from '@/events/ticket-updated-event';
 export * from '@/events/types/order-status';
-export * from '@/events/orders-created-event';
+export * from '@/events/order-created-event';
